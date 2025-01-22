9dbdc58f68ce88be95ba73b6dbefe4d8d771d095
Eric Galvan <lericgalvan@outlook.com>
2025-01-21 17:09:20 -0800

    Initial environment setup

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GrAPE.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
